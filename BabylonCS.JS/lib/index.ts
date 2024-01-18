2cda3ad12dfa1c2c52a03338127dfe9d5a619797
drumath <2237k.drumath@gmail.com>
2024-01-19 03:28:37 +0900

    :sparkles: moduleをセットアップ

--- BabylonCS.JS/lib/index.ts
+++ BabylonCS.JS/lib/index.ts
@@ -1,4 +1,9 @@
 export * from "./Person";
 export * from "./utils";
 
+export * from "./canvasUtil";
+export * from "./engine";
+export * from "./scene";
+export * from "./mesh";
+
 export { setupBabylonCs } from "./setupBabylonCs";

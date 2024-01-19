32e090943c47e5fe00bec31c13fc393a34f5bc37
drumath <2237k.drumath@gmail.com>
2024-01-20 04:19:55 +0900

    :sparkles: scene, engineのC#APIを作ってみた

---
+++ BabylonCS/BabylonCS/Engine.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using BabylonCS.Interop;
+
+namespace BabylonCS;
+
+public interface IEngine
+{
+    void RunRenderLoop(Action callback);
+    void SetupResize();
+}
+
+public class Engine(RenderCanvas renderCanvas, bool antialias) : IEngine
+{
+    internal JSObject EngineObj { get; } = EngineImpl.CreateEngine(renderCanvas.CanvasElement, antialias);
+
+    public void RunRenderLoop(Action callback)
+    {
+        EngineImpl.EngineRunRenderLoop(EngineObj, callback);
+    }
+
+    public void SetupResize()
+    {
+        EngineImpl.EngineSetupResize(EngineObj);
+    }
+}

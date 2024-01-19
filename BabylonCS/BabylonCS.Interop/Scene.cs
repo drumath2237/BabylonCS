8c07269798587d3f53403da8b9873eb9f576c360
drumath <2237k.drumath@gmail.com>
2024-01-20 04:01:47 +0900

    :sparkles: scene, engineのinterop部分のC#クラスを実装

---
+++ BabylonCS/BabylonCS.Interop/Scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace BabylonCS.Interop;
+
+public static partial class SceneImpl
+{
+    [JSImport("scene.createScene", BabylonCSInfo.ModuleName)]
+    public static partial JSObject CreateScene(JSObject engine);
+
+    [JSImport("scene.sceneCreateDefaultCameraOrLight", BabylonCSInfo.ModuleName)]
+    public static partial void SceneCreateDefaultCameraOrLight(
+        JSObject scene,
+        bool createArcRotateCamera,
+        bool replace,
+        bool attachCameraControl
+    );
+
+    [JSImport("scene.sceneRender", BabylonCSInfo.ModuleName)]
+    public static partial void SceneRender(JSObject scene);
+}

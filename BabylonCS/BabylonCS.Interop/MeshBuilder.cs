c86d4db9a25b008a349f3ce3862642021cb3dc22
drumath <2237k.drumath@gmail.com>
2024-01-20 05:06:27 +0900

    :tada: boxが表示された

---
+++ BabylonCS/BabylonCS.Interop/MeshBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace BabylonCS.Interop;
+
+public static partial class MeshBuilderImpl
+{
+    [JSImport("mesh.createBox", BabylonCSInfo.ModuleName)]
+    public static partial JSObject CreateBox(string name, float size);
+
+    [JSImport("mesh.createSphere", BabylonCSInfo.ModuleName)]
+    public static partial JSObject CreateSphere(string name, int segments, float diameter);
+}
--- BabylonCS/Playground/Program.cs
+++ BabylonCS/Playground/Program.cs
@@ -3,7 +3,7 @@ using BabylonCS;
 
 if (!Document.TryGetRenderCanvas("renderCanvas", out var renderCanvas))
 {
-    Console.WriteLine("cannot found canvas");
+    Console.Error.WriteLine("canvas not found");
     return;
 }
 
@@ -11,6 +11,8 @@ var engine = new Engine(renderCanvas, true);
 var scene = new Scene(engine);
 
 scene.CreateDefaultCameraOrLight(true, true, true);
+
+MeshBuilder.CreateBox("box", 0.2f);
 
 engine.SetupResize();
 engine.RunRenderLoop(() => scene.Render());

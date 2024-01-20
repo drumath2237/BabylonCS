db7d7885842931a19d6b5139bde1347d939534b8
drumath <2237k.drumath@gmail.com>
2024-01-20 19:52:40 +0900

    :bug: typo修正によりSphereも生成できるようになった

--- BabylonCS.JS/lib/mesh.ts
+++ BabylonCS.JS/lib/mesh.ts
@@ -3,10 +3,10 @@ import { MeshBuilder } from "@babylonjs/core";
 export const createBox = (name: string, size: number) =>
   MeshBuilder.CreateBox(name, { size });
 
-export const createSphare = (
+export const createSphere = (
   name: string,
   segments: number,
   diameter: number
 ) => MeshBuilder.CreateSphere(name, { segments, diameter });
 
-export const meshModule = { createBox, crateSphere: createSphare };
+export const meshModule = { createBox, createSphere };
